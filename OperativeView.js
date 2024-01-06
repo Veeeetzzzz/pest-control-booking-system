7fe263657b766590a566e60745e2d25a3e5ecae5
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-06 20:04:43 +0000

    Create OperativeView.js

---
+++ OperativeView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OperativeView = () => {
+    return (
+        <div>
+            <h2>Operative Dashboard</h2>
+            {/* Add operative-specific components here */}
+            {/* Example components could be job status update, current assignments, etc. */}
+        </div>
+    );
+};
+
+export default OperativeView;
