0e9fdf5cb0f42d31f9bbe74be73436a59bc653fc
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-06 20:04:28 +0000

    Create ManagerView.js

---
+++ ManagerView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ManagerView = () => {
+    return (
+        <div>
+            <h2>Manager Dashboard</h2>
+            {/* Add manager-specific components here */}
+            {/* Example components could be job assignment, scheduling, reports, etc. */}
+        </div>
+    );
+};
+
+export default ManagerView;
