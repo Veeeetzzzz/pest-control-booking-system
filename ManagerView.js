686c1e21cfd086bb0e29ba485e3df152c821d3a9
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-06 20:10:02 +0000

    Update ManagerView.js

--- ManagerView.js
+++ ManagerView.js
@@ -4,8 +4,25 @@ const ManagerView = () => {
     return (
         <div>
             <h2>Manager Dashboard</h2>
-            {/* Add manager-specific components here */}
-            {/* Example components could be job assignment, scheduling, reports, etc. */}
+            <div className="manager-functions">
+                {/* Placeholder for viewing operatives' diaries */}
+                <div className="view-diaries">
+                    <h3>Operatives' Diaries</h3>
+                    <button onClick={() => {/* logic to view diaries */}}>
+                        View Diaries
+                    </button>
+                    {/* You can add a component or modal here to display the diaries */}
+                </div>
+
+                {/* Placeholder for viewing unassigned jobs */}
+                <div className="view-unassigned-jobs">
+                    <h3>Unassigned Jobs</h3>
+                    <button onClick={() => {/* logic to view unassigned jobs */}}>
+                        View Unassigned Jobs
+                    </button>
+                    {/* Here, you can implement a list or table to show unassigned jobs */}
+                </div>
+            </div>
         </div>
     );
 };
