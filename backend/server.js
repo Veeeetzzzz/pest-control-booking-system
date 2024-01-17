b5e3afb09400d69edd9472b6dc866d0c4e088510
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-17 20:06:54 +0000

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+//Nothing in this file will run until you comment out all the required lines + add your MongoDB credentials.
+//const express = require('express');
+//const mongoose = require('mongoose');
+//const app = express();
+//const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+//app.use(express.json());
+
+// Connect to MongoDB
+//mongoose.connect('your-mongodb-connection-string', { useNewUrlParser: true, useUnifiedTopology: true })
+//  .then(() => console.log('MongoDB Connected'))
+//  .catch(err => console.log(err));
+
+// Define a simple route to test our backend
+//app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the server
+//app.listen(port, () => console.log(`Server running on port ${port}`));
