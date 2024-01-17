768d116968f4aee4d4c9df1fe1cc5a78fc283148
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-17 20:08:13 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-//Nothing in this file will run until you comment out all the required lines + add your MongoDB credentials.
-//const express = require('express');
-//const mongoose = require('mongoose');
-//const app = express();
-//const port = process.env.PORT || 5000;
+/*
+//Nothing in this file will run until you remove lines 1/2 and 21.
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
-//app.use(express.json());
+app.use(express.json());
 
 // Connect to MongoDB
-//mongoose.connect('your-mongodb-connection-string', { useNewUrlParser: true, useUnifiedTopology: true })
-//  .then(() => console.log('MongoDB Connected'))
-//  .catch(err => console.log(err));
+mongoose.connect('your-mongodb-connection-string', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Define a simple route to test our backend
-//app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 // Start the server
-//app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
+*/
