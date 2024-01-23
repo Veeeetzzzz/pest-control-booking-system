17d6c33a43b6c92533430b8569ad0fe895581cfb
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-23 21:17:57 +0000

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-        clientId: "your_client_id", // This is your client ID
+        clientId: "e089d7bc-ed2c-4100-b1ca-475f764262a4", // This is your client ID
         authority: "https://login.microsoftonline.com/your_tenant_id", // Replace YOUR_TENANT_ID with your Azure AD tenant ID
         redirectUri: "http://localhost:3000" // This should match your redirect URI in Azure AD registration
     }
