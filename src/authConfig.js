d7c347e76e96ae794ebecd2eaa9287598b2f1891
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-23 21:18:36 +0000

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,8 @@
 export const msalConfig = {
     auth: {
         clientId: "e089d7bc-ed2c-4100-b1ca-475f764262a4", // This is your client ID
-        authority: "https://login.microsoftonline.com/your_tenant_id", // Replace YOUR_TENANT_ID with your Azure AD tenant ID
-        redirectUri: "http://localhost:3000" // This should match your redirect URI in Azure AD registration
+        authority: "https://login.microsoftonline.com/d7e1ee4c-3a16-49a5-b078-a456b6169744", // Replace YOUR_TENANT_ID with your Azure AD tenant ID
+        redirectUri: "https://pest-control-booking-system.vercel.app/" // This should match your redirect URI in Azure AD registration
     }
 };
 
