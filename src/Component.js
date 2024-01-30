ec22af2ba3e914a827b5947838320055edfd8f0c
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-01-30 20:21:29 +0000

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from './authConfig';
-import './Login.css';
 
 export default function Component() {
   const { instance } = useMsal();
