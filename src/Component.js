817b5ed8b781788ea6de195b79cdecf583cc0270
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-02-03 17:31:43 +0000

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -2,11 +2,60 @@ import React, { useState } from 'react';
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from './authConfig';
 import './Component.css';
+//These imports aren't ready to use yet.
+// import { Client } from "@microsoft/microsoft-graph-client";
+// import ManagerView from "./ManagerView";
+// import OperativeView from "./OperativeView";
 
 export default function Component() {
   const { instance } = useMsal();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // Not yet implemented
+  // const [role, setRole] = useState(null);
+
+// Not yet implemented 
+//   const getGraphClient = (accessToken) => {
+//     return Client.init({
+//       authProvider: (done) => {
+//         done(null, accessToken); // First parameter takes an error if you want to throw one.
+//       },
+//     });
+//   };
+
+//   const getUserDetails = async () => {
+//     try {
+//       const response = await instance.acquireTokenSilent({
+//         ...loginRequest,
+//         account: accounts[0],
+//       });
+
+//       const graphClient = getGraphClient(response.accessToken);
+//       const userDetails = await graphClient.api('/me').get();
+
+//       // Assuming PCAPPROLE comes as a custom claim within the user's details object.
+//       // You might need to check the exact path depending on your setup.
+//       setRole(userDetails.PCAPPROLE); 
+
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (accounts && accounts.length > 0) {
+//       getUserDetails();
+//     }
+//   // Ensure effect only runs once upon component mount
+//   }, [accounts]);
+
+//   // Logic to display views based on role
+//   let contentView = null;
+//   if (role === "Manager") {
+//     contentView = <ManagerView />;
+//   } else if (role === "Operative") {
+//     contentView = <OperativeView />;
+//   }
 
   const handleLogin = (loginType) => {
     if(loginType === "popup") {
