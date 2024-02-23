05d088d21eb45fd5aaf3ae5c1aa397557ca05084
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-02-23 19:43:12 +0000

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useMsal } from '@azure/msal-react';
+import { loginRequest } from './AuthConfig';
 
 //This is not ready yet.
 //To do: Update Avatar button to pull through profile of user who has signed in
 
 const Navbar = () => {
+  const { instance, accounts } = useMsal();
+  const [userAvatar, setUserAvatar] = useState(null);
+
+  useEffect(() => {
+    const fetchUserProfilePhoto = async () => {
+      if (accounts.length > 0) {
+        try {
+          const response = await instance.acquireTokenSilent({
+            ...loginRequest,
+            account: accounts[0],
+          });
+
+          const photoResponse = await fetch('https://graph.microsoft.com/v1.0/me/photo/$value', {
+            headers: new Headers({
+              'Authorization': `Bearer ${response.accessToken}`,
+            }),
+            method: 'GET',
+          });
+
+          if (photoResponse.status === 200) {
+            const imageBlob = await photoResponse.blob();
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setUserAvatar(imageUrl);
+          } else {
+            // Handle the case where the user doesn't have a profile picture
+            // For example, set to a default image
+            setUserAvatar('/path/to/default/avatar.jpg');
+          }
+        } catch (e) {
+          console.error(e);
+          // Handle the case where the token could not be acquired or the network request failed
+        }
+      }
+    };
+
+    fetchUserProfilePhoto();
+  }, [instance, accounts]);
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -16,7 +56,7 @@ const Navbar = () => {
         <div className="dropdown dropdown-end">
           <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-              <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img alt="User avatar" src={userAvatar} />
             </div>
           </div>
           <ul tabIndex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
