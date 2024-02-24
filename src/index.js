b130bffb371b1382a9f6783adfbe5fca77cf4ecd
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-02-24 16:02:00 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './App.css';
 import './globals.css';
-import App from './App'; // This is correct if App.js is alongside index.js in the src directory.
+import App from './App'; //
 
 ReactDOM.render(
   <React.StrictMode>
