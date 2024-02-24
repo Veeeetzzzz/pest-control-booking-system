a96aba87c97a31a426bea92654dc39f68971b0b3
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-02-24 16:11:06 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './globals.css';
 import App from './App';
-//import Component from './Component'; //
 
 ReactDOM.render(
   <React.StrictMode>
