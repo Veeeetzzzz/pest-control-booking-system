29b35f8fb67677cf7ed04d4971012967dda18f40
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-02-24 16:00:11 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './App.css';
-import './global.css';
+import './globals.css';
 import App from './App'; // This is correct if App.js is alongside index.js in the src directory.
 
 ReactDOM.render(
